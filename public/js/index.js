c26619fe7c44fb67621239766664137aed74a70b
Conan <57460454+arcticsurgeon@users.noreply.github.com>
2020-07-10 12:35:12 +0100

    Updates in description...
    Included Username field within form (need to validate still)
    Included error messages within form - prevents form from submitting if fields aren't filled in, also shows messages to user on form
    Included Image preview on form - When user selects an image they can see what it will look like before submitting the form
    An overflow scroll bar for form if goes off page (the idea is for images to be static while form scrolls - I'm thinking of using smaller images either side of the form, maybe photos already uploaded?)

--- public/js/index.js
+++ public/js/index.js
@@ -16,61 +16,64 @@ window.onload = setup;
 const renderDog = (dog) => {
     
     //dog container
-    dogContainer = document.createElement('section')
-    dogContainer.setAttribute('id', 'dog-container')
+    const dogContainer = document.createElement('section') //creates the dogContainer
+    dogContainer.setAttribute('id', 'dog-container') //sets the id for dogContainer
 
-    document.getElementById('container').appendChild(dogContainer);
+    document.getElementById('container').appendChild(dogContainer); //dogContainer is now a child to container
 
+    //dog-photo-container
+    const dogPhotoContainer = document.createElement('div') //creates dogPhotoContainer
+    dogPhotoContainer.setAttribute('id', 'dog-photo') //sets the id for dogPhotoContainer
 
-    const dogPhoto = document.createElement('img');
-    dogPhoto.src = '/photosofdogs/' + dog.imageFileName
-
-    //dog-photo
-    dogPhotoContainer = document.createElement('div')
-    dogPhotoContainer.setAttribute('id', 'dog-photo')
-
-    dogContainer.appendChild(dogPhotoContainer)
-    dogContainer.appendChild(dogPhoto)
-
-    /*
-    //INCLUDE IMAGE HERE
-                <a href = '#'>   <!--When the dogs photo is clicked it will take the user to that dogs profile page-->
-                    <img id = 'photoOfDogAbove' src = 'PhotosOfDog/Manson.jpg' alt = 'Profile Pic'> <!--each photo will show the chosen profile photo of each dog-->
-                </a>
-
-                For guidence^^^^^
-
-    */
-    //name
-    dogNameContainer = document.createElement('p') //creates a p tag
-    dogNameContainer.setAttribute('id', 'dog-name') //sets te ID for created p tag
-
-    dogNameText = document.createTextNode(dog.name) // Create text node
-    dogNameContainer.appendChild(dogNameText);  // appends the p tag to the text node
-    dogPhotoContainer.appendChild(dogNameContainer) // Appends the p tag as a child to the id of dog-photo in html
+    dogContainer.appendChild(dogPhotoContainer) //dogPhotoContainer is now a child of dogContainer
+    
+    //photo-of-dog
+    const dogPhoto = document.createElement('img'); //creates dogPhoto
+    dogPhoto.src = '/photosofdogs/' + dog.imageFileName //sets the correct image
+    dogPhoto.setAttribute('id', 'photo-of-dog-above') // sets the id for dogPhoto
+    dogPhotoContainer.appendChild(dogPhoto) // dogPhoto is now a child of dogPhotoContainer
 
     // voting-options
-    votingContainer = document.createElement('div')
-    votingContainer.setAttribute('id', 'voting-options')
+    const votingContainer = document.createElement('div') //creates votingContainer
+    votingContainer.setAttribute('id', 'voting-options') //sets id for votingContainer
 
-    dogContainer.appendChild(votingContainer)
+    dogContainer.appendChild(votingContainer) //votingContainer is now child of dogContainer
+
+    //name
+    const dogNameContainer = document.createElement('p') //creates dogNameContainer
+    dogNameContainer.setAttribute('id', 'dog-name') //sets the id for dogNameContainer
+
+    const dogNameText = document.createTextNode(dog.name) // Creates dogNameText with correct text node
+    dogNameContainer.appendChild(dogNameText);  // dogNameText is a child of dogNameContainer
+
+    //breed
+    const dogBreedContainer = document.createElement('div') //creates dogBreedContainer
+    dogBreedContainer.setAttribute('id', 'dog-breed') //sets the id for dogBreedContainer
+    const dogBreedText = document.createTextNode(dog.breed) // Creates dogBreedText with correct text node
+
+    dogBreedContainer.appendChild(dogBreedText);  // dogBreedText is a child of dogNameContainer
+
+    //dog Details
+    votingContainer.appendChild(dogNameContainer) // dogNameContainer is a child of votingContainer
+    votingContainer.appendChild(dogBreedContainer) // dogBreedContainer is a child of votingContainer
+
 
     //votes
-    voteNumber = document.createElement('p')
-    voteNumber.setAttribute('id', 'total-votes')
-    voteNumber.textContent = 'Total Votes = '
+    const voteNumber = document.createElement('p') //creates voteNumber
+    voteNumber.setAttribute('id', 'total-votes') //sets the id for voteNumber
+    voteNumber.textContent = 'Total Votes = ' //Adds a set text for voteNumber
 
-    voteNode = document.createTextNode(dog.votes)
-    voteNumber.appendChild(voteNode)
-    votingContainer.appendChild(voteNumber)
+    const voteNode = document.createTextNode(dog.votes) // creates voteNode (the number of votes for dog)
+    voteNumber.appendChild(voteNode) // voteNode is a child of voteNumber
+    votingContainer.appendChild(voteNumber) //voteNumber is a child of votingConatiner
 
     //button
-    const voteForDogAbove = document.createElement('button')
-    voteForDogAbove.setAttribute('id', 'btn')
-    voteForDogAbove.textContent = 'Vote For Above Dog'
-    votingContainer.appendChild(voteForDogAbove)
+    const voteForDogAbove = document.createElement('button') //create the voteForDogAbove button
+    voteForDogAbove.setAttribute('id', 'btn') //sets to id for voteForDogAbove
+    voteForDogAbove.textContent = 'Vote For Above Dog' //adds a set text for voteForDogAbove
+    votingContainer.appendChild(voteForDogAbove) //voteForDogAbove is a child of votingConatiner
 }
-document.getElementById('saveDog').addEventListener('click', voteForDog)
+document.getElementById('btn').addEventListener('click', voteForDog)
 
 const voteForDog = () => {
 
--- public/js/sign-up.js
+++ public/js/sign-up.js
@@ -3,24 +3,87 @@ window.onload = () => {
   document.getElementById('saveDog').addEventListener('click', signup)
 };
 
-const signup = async (ev) => {
 
+const signup = async (ev) => {
   ev.preventDefault();
+  //fields that need inputs
+  let messages = []
+  //error elements
+  const errorMessage = document.getElementById('error-message')
+  const errorImage = document.getElementById('error-image')
+  const errorUsername = document.getElementById('error-username')
+  const errorName = document.getElementById('error-name')
+  const errorBreed = document.getElementById('error-breed')
+  const errorDob = document.getElementById('error-dob')
+  const errorColour = document.getElementById('error-colour')
+
+  //Values
+  const file = document.getElementById('photo')
+  const username = document.getElementById('username')
+  const name = document.getElementById('name');
+  const breed = document.getElementById('breed');
+  const dob = document.getElementById('dob');
+  const colour = document.getElementById('colour');
 
   let form = new FormData();
-  const file = document.getElementById('photo')
-  console.log('file', file)
-  // check that files.files contains something - i imagine it will need to check the .length of that to be greater than 0
-  // if they haven't added a file, tell them they need to, and return from the function
-  console.log('file.files[0]', file.files[0])
-  form.append('photo', file.files[0], file.files[0].name);
-  // add in other form fields here...
 
-  // also check name is valid string...
+  //image
+  if(file.files.length > 0){
+    errorImage.innerText = ''
+    form.append('photo', file.files[0], file.files[0].name);
+  } else{
+    errorImage.innerText = 'Image is required'
+    messages.push('image')
+  }
+   //username
+   if(username.value === '' || username.value == null){
+    errorUsername.innerText= 'Username is required'
+    messages.push('Username')
+  } else{
+    errorUsername.innerText = ''
+    form.set('Username', username.value);
+  }
+  //name
+  if(name.value === '' || name.value == null){
+    errorName.innerText= 'Name is required'
+    messages.push('name')
+  } else{
+    errorName.innerText = ''
+    form.set('name', name.value);
+  }
+  //breed
+  if(breed.value === '' || breed.value == null){
+    errorBreed.innerText = 'Breed is required'
+    messages.push('breed')
+  } else{
+    errorBreed.innerText = ''
+    form.set('breed', breed.value);
+  }  
+  //dob
+  if(dob.value === '' || dob.value == null){
+    errorDob.innerText = 'Date Of Birth is required'
+    messages.push('dob')
+  } else{
+    errorDob.innerText = ''
+    form.set('dob', dob.value);
+  }  
+  //colour
+  if(colour.value === '' || colour.value == null){
+    errorColour.innerText= 'Colour is required'
+    messages.push('colour')
+  } else{
+    errorColour.innerText = ''
+    form.set('colour', colour.value);
+  }
+  //error message
+  console.log(messages)
+  if(messages.length > 0){
+    errorMessage.innerText = 'Please fill in required fields'
+    return
+  } else{
+    errorMessage.innerText = ''
+  }
 
-  form.set('name', document.getElementById('name').value);
-  // don't need to set votes, as it's always zero, so we set in the server
-  form.set('votes', 0);
   // add a try/catch here - then check response status code is 200, and then display message/redirect to the homepage
   const response = await axios.post('/dog', form, {
     headers: {
@@ -31,4 +94,18 @@ const signup = async (ev) => {
   });
   console.log('response', response)
 
+  window.location.assign('index.html');
+  document.getElementById('form').reset();
+}
+
+const showPreview = (event) => {
+  if(event.target.files.length > 0){
+    const src = URL.createObjectURL(event.target.files[0]);
+    const preview = document.getElementById("image-preview-image");
+    preview.src = src;
+    preview.style.display = "block";
+    preview.style.maxHeight = "250px";
+    preview.style.maxWidth = "250px";
+    preview.style.borderRadius = "50%";
+  }
 }
